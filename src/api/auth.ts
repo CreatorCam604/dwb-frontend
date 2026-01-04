50ade3468cc61db5d7f834665245d195af469833
Cameron Brighton <cameronbrighton240@gmail.com>
2026-01-04 18:06:57 +0200

    Frontend changes

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,9 @@
 import { api } from "./http";
 
 export async function login(email: string, password: string) {
-  const res = await api.post("/auth/login", { email, password });
-  return res.data.token as string;
+  const res = await api.post("/auth/login", {
+    email,
+    password,
+  });
+  return res.data;
 }
--- src/api/http.ts
+++ src/api/http.ts
@@ -11,14 +11,3 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-api.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
