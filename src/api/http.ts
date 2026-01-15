843440c9d97601d32987ecdd01e7294347766ec0
Cameron Brighton <cameronbrighton240@gmail.com>
2026-01-15 22:13:44 +0200

    3

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
